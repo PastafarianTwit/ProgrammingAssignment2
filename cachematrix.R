d6ac1820b3ee9c5f53a1357a893aea074f5f25da
PastafarianTwit <1152350+PastafarianTwit@users.noreply.github.com>
2017-10-20 13:13:39 -0500

    Update cachematrix.R
    Coded up the assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file stores a cache of a matrix inversion and reuses the cached
+## version if it's already been calculated.  This reduces the overhead
+## required for large matrices.
 
-## Write a short comment describing this function
+## a list of helper functions for caching the matrix inversion
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	s <- NULL
+	set <- function(y) {
+		x <<- y
+		s <<- NULL
+	}
+	get <- function() x
+	setSolve <- function(solve) s <<- solve
+	getSolve <- function() s
+	list(set = set, get = get, setmean = setmean, getmean = getmean)
 }
 
 
-## Write a short comment describing this function
+## This will perform the matrix inversion, checking the cache first. 
+## If no cache is found, it will run the calculation and store to cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		s <- x$getSolve()
+		if(!is.null(s)) {
+			message("getting cached data")
+			return(s)
+		}
+		data <- x$get()
+		s <- solve(data, ...)
+		x$setSolve(s)
+		s
 }
